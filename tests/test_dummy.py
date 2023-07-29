c518212eec6391573814b0e476af329372156cde
Jamie Mortensen <16mortensenj@gmail.com>
2023-07-29 18:02:40 -0400

    removed failing test

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -4,8 +4,8 @@ import unittest
 
 class TestDummy(unittest.TestCase):
 
-    def test_fails(self):
-        self.assertTrue(False)
+    # def test_fails(self):
+    #     self.assertTrue(False)
 
     def test_passes(self):
         self.assertTrue(True)
