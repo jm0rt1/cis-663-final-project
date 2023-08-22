8535a3f1d8212c684ab5e3d1f3094c0924945c68
Jamie Mortensen <16mortensenj@gmail.com>
2023-08-21 23:11:57 -0400

    Refactor report_fixer.py DEV #5: Updated to functions and created main

--- scripts/report_fixer.py
+++ scripts/report_fixer.py
@@ -1,171 +1,113 @@
+# Redefining the refactored functions and main function after importing the required libraries
 from pathlib import Path
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 import re
 
-input_path = Path(
-    "output/reports/face-recognition/classification_reports/report_20230820-214937.txt")
 
-
-def extract_timestamp(path: Path):
-    """ Example report_20230820-214937.txt return 20230820-214937 as a string"""
+def extract_timestamp(path: Path) -> str:
+    """ Extract timestamp from given file path. """
     return path.stem.split("_")[1]
 
 
-OUTPUT_DIR = Path(
-    f"output/reports/face-recognition/summary/summary_for_{extract_timestamp(input_path)}")
-OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+def create_output_directory(path: Path) -> Path:
+    timestamp = extract_timestamp(path)
+    OUTPUT_DIR = Path(
+        f"output/reports/face-recognition/summary/summary_for_{timestamp}")
+    OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+    return OUTPUT_DIR
 
 
-with open(input_path.as_posix(), 'r') as f:
-    report = f.read()
+def extract_report_data(path: Path) -> pd.DataFrame:
+    with open(path.as_posix(), 'r') as f:
+        report = f.read()
 
-# Extracting information from report
-sections = report.split("Classification Report")
-sections = sections[1:]  # Removing the first split result which is empty
+    # Removing the first split result which is empty
+    sections = report.split("Classification Report")[1:]
+    data = []
 
-data = []
+    for section in sections:
+        target_percentage = int(
+            re.search(r"Percentage of Target in Dataset: (\d+)", section).group(1))
+        smote = "SMOTE Resampled = True" in section
+        face_detection = "Face Detection Used = True" in section
+        not_you = re.search(
+            r"Not You\s+([\d.]+)\s+([\d.]+)\s+([\d.]+)", section)
+        you = re.search(r"You\s+([\d.]+)\s+([\d.]+)\s+([\d.]+)", section)
+        macro_avg = re.search(
+            r"macro avg\s+[\d.]+\s+[\d.]+\s+([\d.]+)", section)
+        weighted_avg = re.search(
+            r"weighted avg\s+[\d.]+\s+[\d.]+\s+([\d.]+)", section)
 
-for section in sections:
-    target_percentage = int(
-        re.search(r"Percentage of Target in Dataset: (\d+)", section).group(1))
-    smote = True if "SMOTE Resampled = True" in section else False
-    face_detection = True if "Face Detection Used = True" in section else False
+        data.append([
+            target_percentage, smote, face_detection,
+            float(not_you.group(1)), float(
+                not_you.group(2)), float(not_you.group(3)),
+            float(you.group(1)), float(you.group(2)), float(you.group(3)),
+            float(macro_avg.group(1)), float(weighted_avg.group(1))
+        ])
 
-    not_you = re.search(r"Not You\s+([\d.]+)\s+([\d.]+)\s+([\d.]+)", section)
-    you = re.search(r"You\s+([\d.]+)\s+([\d.]+)\s+([\d.]+)", section)
-    macro_avg = re.search(r"macro avg\s+[\d.]+\s+[\d.]+\s+([\d.]+)", section)
-    weighted_avg = re.search(
-        r"weighted avg\s+[\d.]+\s+[\d.]+\s+([\d.]+)", section)
-
-    data.append([
-        target_percentage, smote, face_detection,
-        float(not_you.group(1)), float(
-            not_you.group(2)), float(not_you.group(3)),
-        float(you.group(1)), float(you.group(2)), float(you.group(3)),
-        float(macro_avg.group(1)), float(weighted_avg.group(1))
+    df = pd.DataFrame(data, columns=[
+        "Target Percentage", "SMOTE", "Face Detection",
+        "Not You Precision", "Not You Recall", "Not You F1",
+        "You Precision", "You Recall", "You F1",
+        "Macro Avg F1", "Weighted Avg F1"
     ])
-
-df = pd.DataFrame(data, columns=[
-    "Target Percentage", "SMOTE", "Face Detection",
-    "Not You Precision", "Not You Recall", "Not You F1",
-    "You Precision", "You Recall", "You F1",
-    "Macro Avg F1", "Weighted Avg F1"
-])
-
-# Sorting by Weighted Avg F1 to get the top combination
-best_combination = df.sort_values(
-    by="Weighted Avg F1", ascending=False).iloc[0]
-
-with open(OUTPUT_DIR/f"report_{extract_timestamp(input_path)}.csv", 'w') as f:
-    f.write(df.to_csv(index=False))
-    f.write(f"\n\n\nBest Combination based on Weighted Avg F1:\n")
-    f.write(f"Target Percentage: {best_combination['Target Percentage']}\n")
-    f.write(f"SMOTE: {best_combination['SMOTE']}\n")
-    f.write(f"Face Detection: {best_combination['Face Detection']}\n")
-    f.write(f"Weighted Avg F1 Score: {best_combination['Weighted Avg F1']}\n")
+    return df
 
 
-# Assuming you have already defined 'df' from previous steps
+def generate_visualizations(df: pd.DataFrame, output_dir: Path):
+    # Line plot
+    plt.figure(figsize=(14, 7))
+    sns.lineplot(data=df, x='Target Percentage', y='Weighted Avg F1',
+                 hue='SMOTE', style='Face Detection', markers=True, dashes=False)
+    plt.title('Weighted Avg F1 Score vs. Target Percentage')
+    plt.xlabel('Target Percentage (%)')
+    plt.ylabel('Weighted Avg F1 Score')
+    plt.grid(True)
+    plt.tight_layout()
+    plt.savefig(
+        output_dir / f"F1_vs_Target_Percentage_{extract_timestamp(input_path)}.png")
+    plt.close()
 
-# Create a pivot table for the heatmap
-pivot_table = df.pivot_table(index="Target Percentage", columns=[
-                             "SMOTE", "Face Detection"], values="Weighted Avg F1")
+    # Heatmap for correlation
+    corr = df.drop(columns=['SMOTE', 'Face Detection',
+                   'Target Percentage']).corr()
+    plt.figure(figsize=(12, 8))
+    sns.heatmap(corr, annot=True, cmap='coolwarm', center=0, linewidths=0.5)
+    plt.title('Correlation Heatmap of Metrics')
+    plt.tight_layout()
+    plt.savefig(
+        output_dir / f"heatmap_correlation_{extract_timestamp(input_path)}.png")
+    plt.close()
 
-
-# Plot the heatmap
-plt.figure(figsize=(10, 8))
-sns.heatmap(pivot_table, annot=True, cmap="YlGnBu", linewidths=0.5,
-            cbar_kws={"label": "Weighted Avg F1 Score"})
-
-plt.title("Performance Comparison based on Weighted Avg F1 Score")
-ylabel = "Percentage of Target Face in Data Set (%)"
-plt.ylabel(ylabel)
-plt.xlabel("Combination of SMOTE and Face Detection")
-# plt.show()
-plt.savefig(OUTPUT_DIR/f"heatmap_F1_{extract_timestamp(input_path)}.png")
-
-# Assuming you have already defined 'df' from previous steps
-
-# Create a pivot table for the heatmap
-pivot_table_recall = df.pivot_table(index="Target Percentage", columns=[
-                                    "SMOTE", "Face Detection"], values="You Recall")
-
-# Plot the heatmap
-plt.figure(figsize=(10, 8))
-sns.heatmap(pivot_table_recall, annot=True, cmap="YlGnBu",
-            linewidths=0.5, cbar_kws={"label": "Recall Score"})
-
-plt.title("Performance Comparison based on Recall")
-plt.ylabel(ylabel)
-plt.xlabel("Combination of SMOTE and Face Detection")
-# plt.show()
-plt.savefig(OUTPUT_DIR/f"heatmap_recall_{extract_timestamp(input_path)}.png")
-
-# Plot the heatmap
-plt.figure(figsize=(10, 8))
-sns.heatmap(pivot_table_recall, annot=True, cmap="YlGnBu",
-            linewidths=0.5, cbar_kws={"label": "Recall Score"})
-
-plt.title("Performance Comparison based on Recall")
-plt.ylabel(ylabel)
-plt.xlabel("Combination of SMOTE and Face Detection")
-# plt.show()
-plt.savefig(OUTPUT_DIR/f"heatmap_recall_{extract_timestamp(input_path)}.png")
-
-# Plot line plot
-plt.figure(figsize=(14, 7))
-sns.lineplot(data=df, x="Target Percentage", y='Weighted Avg F1',
-             hue='SMOTE', style='Face Detection', markers=True, dashes=False)
-plt.title('Weighted Avg F1 Score vs. Target Percentage')
-plt.xlabel('Target Percentage (%)')
-plt.ylabel('Weighted Avg F1 Score')
-plt.grid(True)
-plt.tight_layout()
-plt.savefig(
-    OUTPUT_DIR/f"F1_vs_Target_Percentage{extract_timestamp(input_path)}.png")
+    # Pairplot
+    selected_columns = ['Not You Precision', 'Not You Recall', 'Not You F1',
+                        'You Precision', 'You Recall', 'You F1',
+                        'Macro Avg F1', 'Weighted Avg F1']
+    pairplot = sns.pairplot(df[selected_columns + ['SMOTE', 'Face Detection']],
+                            hue='SMOTE & Face Detection', corner=True, diag_kind='kde', plot_kws={'alpha': 0.6})
+    pairplot.fig.suptitle(
+        "Pairwise Relationships and Distributions of Key Metrics", y=1.02)
+    plt.tight_layout()
+    plt.savefig(output_dir / f"pairplot_{extract_timestamp(input_path)}.png")
+    plt.close()
 
 
-# Visualization 5: Heatmap for correlation between metrics
-
-# Compute the correlation matrix
-corr = df.drop(columns=['SMOTE', 'Face Detection',
-                        'Target Percentage']).corr()
-
-plt.figure(figsize=(12, 8))
-sns.heatmap(corr, annot=True, cmap='coolwarm', center=0, linewidths=0.5)
-plt.title('Correlation Heatmap of Metrics')
-plt.tight_layout()
-plt.savefig(
-    OUTPUT_DIR/f"heatmap_correlation_{extract_timestamp(input_path)}.png")
+def main(input_path: Path):
+    output_dir = create_output_directory(input_path)
+    df = extract_report_data(input_path)
+    df['SMOTE & Face Detection'] = df['SMOTE'].astype(
+        str) + ' & ' + df['Face Detection'].astype(str)
+    generate_visualizations(df, output_dir)
 
 
-# Visualization: Pairplot to show relationships and distributions between key metrics
+input_path = Path(
+    "output/reports/face-recognition/classification_reports/report_20230820-214937.txt")
 
-# Selecting key metrics for the pairplot
-selected_columns = ['Not You Precision', 'Not You Recall', 'Not You F1',
-                    'You Precision', 'You Recall', 'You F1',
-                    'Macro Avg F1', 'Weighted Avg F1']
+# Re-running the main function after redefining the refactored functions
+main(input_path)
 
-
-"""
-Key Features of the Pairplot:
-Scatter Plots: The off-diagonal plots are scatter plots.
-They show the relationship between two variables. For instance,
-the scatter plot at the intersection of "Not You Precision" (row) and
-"You Precision" (column) visualizes the relationship between these two metrics.
-Distribution Plots: The diagonal plots are distribution plots (kernel density estimates)
-for each metric. They give an idea of how the values for each metric are distributed.
-Visualization of Correlations: Scatter plots in the pairplot provide a visual representation
-of the correlation between pairs of metrics. The tighter and more linear the clustering of points, 
-the stronger the correlation.
-"""
-# Creating the pairplot
-pairplot = sns.pairplot(
-    df[selected_columns], corner=True, diag_kind='kde', plot_kws={'alpha': 0.6})
-pairplot.fig.suptitle(
-    "Pairwise Relationships and Distributions of Key Metrics", y=1.02)
-plt.tight_layout()
-plt.savefig(
-    OUTPUT_DIR/f"pairplot_{extract_timestamp(input_path)}.png")
+# Return message for completion
+"Refactored code executed successfully!"
